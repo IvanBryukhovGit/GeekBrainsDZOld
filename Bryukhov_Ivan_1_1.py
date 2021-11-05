d6226de35552080c73ffba4b118463e9c8534294
IvanBryukhovGit <i.bryuxov@mail.ru>
2021-11-05 14:42:13 +0300

    Added function for task 1.1

--- Bryukhov_Ivan_1_1.py
+++ Bryukhov_Ivan_1_1.py
@@ -1,21 +1,63 @@
+def my_time_sek (num):
+    string = ''
+    if num in range(10, 21):
+        string = 'Секунд'
+    elif num % 10 == 1:
+        string = 'Секунда'
+    elif num % 10 in range(2, 5):
+        string = 'Секунды'
+    else:
+        string = 'Секунд'
+    return string
+def my_time_min (num):
+    string = ''
+    if num in range(10, 21):
+        string = 'Минут'
+    elif num % 10 == 1:
+        string = 'Минута'
+    elif num % 10 in range(2, 5):
+        string = 'Минуты'
+    else:
+        string = 'Минут'
+    return string
+def my_time_hour (num):
+    string = ''
+    if num in range(10, 21):
+        string = 'Часов'
+    elif num % 10 == 1:
+        string = 'Час'
+    elif num % 10 in range(2, 5):
+        string = 'Часа'
+    else:
+        string = 'Часов'
+    return string
+def my_time_day(num):
+    string = ''
+    if num in range(10, 21):
+        string = 'Дней'
+    elif num % 10 == 1:
+        string = 'День'
+    elif num % 10 in range(2, 5):
+        string = 'Дня'
+    else:
+        string = 'Дней'
+    return string
+
 duration = int(input('Введите число: '))
 if duration <= 59:
-    print(duration, 'секунд')
+    print(duration, my_time_sek(duration))
 elif duration <= 3599:
     m = duration // 60
     s = duration % 60
-    print(m, 'минут', s, 'секунд')
+    print(m, my_time_min(m), s, my_time_sek(s))
 elif duration <= 86399:
     h = duration // 3600
     m = (duration - h * 3600) // 60
     s = duration % 60
-    print(h, 'час', m, 'минут', s, 'секунд')
+    print(h, my_time_hour(h), m, my_time_min(m), s, my_time_sek(s))
 else:
     d = duration // 86400
     h = (duration - d * 86400) // 3600
     m = (duration - (d * 86400 + h * 3600)) // 60
     s = duration % 60
-    print(d, 'дней', h, 'час', m, 'минут', s, 'секунд')
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(d, my_time_day(d), h, my_time_hour(h), m, my_time_min(m), s, my_time_sek(s))
